9a9dd0a63e8be7b000d5aa648ae11b6ceaeb84d8
André Vega <ventaslederer@hotmail.com>
2022-02-01 00:36:34 -0600

    Added Css classes to generated html

--- utils/generatehtml.js
+++ utils/generatehtml.js
@@ -1,16 +1,18 @@
 function createManager(data) {
   const managerData = data.filter(manager => ("Manager" === manager.getRole()))
   return `
-    <div>
-      <h1>${managerData[0].getName()}</h1>
-      <i class="fas fa-mug-hot"></i>
-      <h2>${managerData[0].getRole()}</h2>
-      <ul>
-        <li>ID: ${managerData[0].getId()}</li>
-        <li>Email: <a href="mailto:${managerData[0].getEmail()}">${managerData[0].getEmail()}</a></li>
-        <li>Office Number: ${managerData[0].getOffice()}</li>
-      </ul>
-    </div>  
+      <div class="shadow-2xl bg-gray-200">
+        <div class="p-4 bg-blue-500 text-white text-2xl px-8">
+          <h1>${managerData[0].getName()}</h1>
+        <i class="fas fa-mug-hot inline-block"></i>
+        <h2 class="inline-block">${managerData[0].getRole()}</h2>
+        </div>
+        <ul class="m-4 my-6 text-lg leading-20 bg-white" class="p-4">
+          <li class="p-4 py-2 border-b-2 border-gray-300">ID: ${managerData[0].getId()}</li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">Email: <a class=" text-blue-700 text-bold pointer" href="mailto:${managerData[0].getEmail()}">${managerData[0].getEmail()}</a></li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">Office Number: ${managerData[0].getOffice()}</li>
+        </ul>
+      </div>    
   `
 }
 // COTINUE ADDING ICONS FOR ENGINEER AND INTERN AND IN GENERAL FINISH ALL HTML AND CSS CODE
@@ -19,15 +21,18 @@ function createEngineer(data) {
   const allEngineersData = []
   engineerData.forEach(engineer => {
     const html = `
-    <div>
-      <h1>${engineer.getName()}</h1>
-      <h2>${engineer.getRole()}</h2>
-      <ul>
-        <li>ID: ${engineer.getId()}</li>
-        <li>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-        <li>GitHub: <a href="https://github.com/${engineer.getGithub()}/" target="_blank">/${engineer.getGithub()}<a></li>
-      </ul>
-    </div>
+      <div class=" shadow-2xl bg-gray-200">
+        <div class="p-4 bg-blue-500 text-white text-2xl px-8">
+          <h1>${engineer.getName()}</h1>
+          <i class="fas fa-glasses"></i>
+          <h2 class="inline">${engineer.getRole()}</h2>
+        </div>
+        <ul class="m-4 my-6 text-lg leading-20 bg-white">
+          <li class="p-4 py-2 border-b-2 border-gray-300">ID: ${engineer.getId()}</li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">Email: <a class=" text-blue-700 text-bold pointer" href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">GitHub: <a class=" text-blue-700 text-bold pointer" href="https://github.com/${engineer.getGithub()}/" target="_blank">/${engineer.getGithub()}</a></li>
+        </ul>
+      </div>
     `
     allEngineersData.push(html)
   });
@@ -39,15 +44,18 @@ function createIntern(data) {
   const allinternsData = []
   internData.forEach(intern => {
     const html = `
-    <div>
-      <h1>${intern.getName()}</h1>
-      <h2>${intern.getRole()}</h2>
-      <ul>
-        <li>ID: ${intern.getId()}</li>
-        <li>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-        <li>School: ${intern.getSchool()}</li>
-      </ul>
-    </div>
+      <div class=" shadow-2xl bg-gray-200">
+        <div class="p-4 bg-blue-500 text-white text-2xl px-8">
+          <h1>${intern.getName()}</h1>
+          <i class="fas fa-user-graduate"></i>
+          <h2 class="inline">${intern.getRole()}</h2>
+        </div>
+        <ul class="m-4 my-6 text-lg leading-20 bg-white">
+          <li class="p-4 py-2 border-b-2 border-gray-300">ID: ${intern.getId()}</li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">Email: <a class=" text-blue-700 text-bold pointer" href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+          <li class="p-4 py-2 border-b-2 border-gray-300">School: ${intern.getSchool()}</li>
+        </ul>
+      </div>
     `
     allinternsData.push(html)
   });
@@ -69,7 +77,7 @@ function generateHtml(data) {
       <h1 class="text-white text-3xl">My Team</h1>
     </section>
   
-    <section id="cardsSection" class="grid sm:grid-cols-3">
+    <section id="cardsSection" class="grid md:grid-cols-3 gap-4 p-4 justify-center align-center">
       ${createManager(data)}
       ${createEngineer(data)}
       ${createIntern(data)}
